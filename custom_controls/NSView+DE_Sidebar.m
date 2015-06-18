aed160812917cb21550ccc2cf2891fd4a14fae56
Dave P <itsmedavep@gmail.com>
2015-06-17 21:27:12 -0500

    Initial commit.  Project setup.

---
+++ custom_controls/NSView+DE_Sidebar.m
@@ -0,0 +1,13 @@
+//
+//  NSView+DE_Sidebar.m
+//  custom_controls
+//
+//  Created by David Palmer on 6/17/15.
+//  Copyright (c) 2015 discode enterprises. All rights reserved.
+//
+
+#import "NSView+DE_Sidebar.h"
+
+@implementation NSView (DE_Sidebar)
+
+@end
