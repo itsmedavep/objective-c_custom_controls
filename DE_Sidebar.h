c90bae72b641615c33632a4a33b532aff5b1636e
Dave P <itsmedavep@gmail.com>
2015-06-18 06:41:06 -0500

    More side panel work.

--- DE_Sidebar.h
+++ DE_Sidebar.h
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015 discode enterprises. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
 
 @interface DE_Sidebar : NSView
--- DE_Sidebar.m
+++ DE_Sidebar.m
@@ -12,8 +12,26 @@
 
 - (void)drawRect:(NSRect)frame {
     
-    [[NSColor darkGrayColor] set];
-    [NSBezierPath fillRect:frame];
+    
+    //// Shadow Declarations
+    NSShadow* shadowSidePanel = NSShadow.alloc.init;
+    [shadowSidePanel setShadowColor: NSColor.blackColor];
+    [shadowSidePanel setShadowOffset: NSMakeSize(0.1, 0.1)];
+    [shadowSidePanel setShadowBlurRadius: 15];
+    
+    //// Rectangle Drawing
+    NSBezierPath* rectanglePath = [NSBezierPath bezierPathWithRect: NSMakeRect(NSMinX(frame) + 0.5, NSMinY(frame) - 3.5, 39, 105)];
+    [NSGraphicsContext saveGraphicsState];
+    [shadowSidePanel set];
+    [NSColor.darkGrayColor setFill];
+    [rectanglePath fill];
+    [NSGraphicsContext restoreGraphicsState];
+    
+    [NSColor.blackColor setStroke];
+    [rectanglePath setLineWidth: 4];
+    [rectanglePath stroke];
+    
+    [self setNeedsDisplay:YES];
     
     // Drawing code here.
 }
