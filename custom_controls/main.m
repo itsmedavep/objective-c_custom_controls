32f971f9df6c813e8161f6ed66216eceede99b37
Dave P <itsmedavep@gmail.com>
2015-06-17 20:57:04 -0500

    Initial Commit

---
+++ custom_controls/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  custom_controls
+//
+//  Created by David Palmer on 6/17/15.
+//  Copyright (c) 2015 discode enterprises. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
---
+++ custom_controlsTests/custom_controlsTests.m
@@ -0,0 +1,40 @@
+//
+//  custom_controlsTests.m
+//  custom_controlsTests
+//
+//  Created by David Palmer on 6/17/15.
+//  Copyright (c) 2015 discode enterprises. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface custom_controlsTests : XCTestCase
+
+@end
+
+@implementation custom_controlsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
